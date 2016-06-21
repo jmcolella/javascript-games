f908bb19aeb4389e9d0c0c3470010f309a1a2e2c
jmcolella <colella.john@gmail.com>
2016-06-21 15:18:51 -0400

    add basic game logic to place markers and add underscore.js library

--- tic-tac-toe/application.js
+++ tic-tac-toe/application.js
@@ -2,17 +2,18 @@ $(document).ready(function(){
   var game = new Game();
   $("#board").on("click", "td.plain", function(e) {
     e.preventDefault();
-    debugger;
+    var cellIndex = $(e.target).index();
+    var rowIndex = $(e.target).parent().index();
     if ($(e.target).hasClass("plain")) {
       if ( game.moves % 2 === 0 ) {
-        $(e.target).toggleClass("x");
-        $(e.target).removeClass("plain");
-        x = true;
+        debugger;
+        game.makeMove(cellIndex, rowIndex, game.player1);
+        game.placeMarker(e.target, game.player1);
       } else {
-        $(e.target).toggleClass("o");
-        $(e.target).removeClass("plain");
-        x = false
+        game.makeMove(cellIndex, rowIndex, game.player2);
+        game.placeMarker(e.target, game.player2);
       };
+      game.moves += 1
     };
   });
 
--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -8,6 +8,40 @@ function Game() {
   this.winner = [];
 };
 
-// Game.prototype.makeMove = function(targetIndex, rowIndex, player) {
+Game.prototype.makeMove = function(targetIndex, rowIndex, player) {
+  for( var i = 0; i < this.board.length; i++ ) {
+    for( var j = 0; j < this.board[i].length; j++ ) {
+      if( rowIndex === i && targetIndex === j ) {
+        if( player === this.player1 ) {
+          this.board[i][j] = "X";
+        } else if( player == this.player2 ) {
+          this.board[i][j] = "O";
+        };
+      };
+    };
+  };
+};
 
-// };
+Game.prototype.placeMarker = function( target, player ) {
+  $(target).addClass("marker");
+   $(target).removeClass("plain");
+  if( player === this.player1 ) {
+    $(target).text("X");
+  } else if( player === this.player2 ) {
+    $(target).text("O");
+  };
+};
+
+Game.prototype.isWon = function() {
+  this.horizontalWinner( this.board) );
+};
+
+Game.prototype.horizontalWinner = function( board ) {
+  board.forEach( function( row ) {
+    if( row.join("").includes("XXX") ) {
+      this.winner.push( this.player1 )
+    } else if ( row.join("").includes("OOO") ) {
+      this.winner.push( this.player2 )
+    };
+  });
+};

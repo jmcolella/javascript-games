18dafdfcd36dc650253baeb0423445f13ca86893
jmcolella <colella.john@gmail.com>
2016-06-21 15:58:32 -0400

    complete win logic for tic tac toe

--- tic-tac-toe/application.js
+++ tic-tac-toe/application.js
@@ -6,16 +6,19 @@ $(document).ready(function(){
     var rowIndex = $(e.target).parent().index();
     if ($(e.target).hasClass("plain")) {
       if ( game.moves % 2 === 0 ) {
-        debugger;
         game.makeMove(cellIndex, rowIndex, game.player1);
         game.placeMarker(e.target, game.player1);
+        if( game.isWon() === game.player1 ) {
+          $("#player1-win-message").show();
+        };
       } else {
         game.makeMove(cellIndex, rowIndex, game.player2);
         game.placeMarker(e.target, game.player2);
+        if( game.isWon() === game.player2 ) {
+          $("#player2-win-message").show();
+        };
       };
       game.moves += 1
     };
   });
-
-
 });
--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -8,7 +8,7 @@ function Game() {
   this.winner = [];
 };
 
-Game.prototype.makeMove = function(targetIndex, rowIndex, player) {
+Game.prototype.makeMove = function( targetIndex, rowIndex, player ) {
   for( var i = 0; i < this.board.length; i++ ) {
     for( var j = 0; j < this.board[i].length; j++ ) {
       if( rowIndex === i && targetIndex === j ) {
@@ -33,15 +33,44 @@ Game.prototype.placeMarker = function( target, player ) {
 };
 
 Game.prototype.isWon = function() {
-  this.horizontalWinner( this.board) );
+  this.horizontalWinner( this.board );
+  this.verticalWinner( this.board );
+  this.diagonalWinner( "left" );
+  this.diagonalWinner();
+  if( this.winner[0] === this.player1 ) {
+    return this.player1
+  } else if( this.winner[0] === this.player2 ) {
+    return this.player2
+  };
 };
 
 Game.prototype.horizontalWinner = function( board ) {
   board.forEach( function( row ) {
     if( row.join("").includes("XXX") ) {
-      this.winner.push( this.player1 )
+      this.winner.push( this.player1 );
     } else if ( row.join("").includes("OOO") ) {
-      this.winner.push( this.player2 )
+      this.winner.push( this.player2 );
     };
-  });
+  }.bind(this));
+};
+
+Game.prototype.verticalWinner = function( board ) {
+  var unzipBoard = _.unzip( board );
+  this.horizontalWinner( unzipBoard );
+};
+
+Game.prototype.rotate = function( array, n ) {
+  return array.slice(n, array.length).concat(array.slice(0, n));
+}
+
+Game.prototype.diagonalWinner = function( direction ) {
+  var rotateArray = []
+  this.board.forEach( function( row, index ) {
+    if( direction === "left" ) {
+      rotateArray.push( this.rotate( row, -(index + 1) ) );
+    } else {
+      rotateArray.push( this.rotate( row, (index + 1) ) );
+    }
+  }.bind(this));
+  this.verticalWinner( rotateArray );
 };
